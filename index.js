1da748cb65651cb1db99c15568ef6a271977c88c
denniskimotho <denniskimotho222@gmail.com>
2023-10-05 08:39:43 +0300

    Initial Message

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 =2
+let num2 = 31
+
+let num3 = 9
+let num4 = 5
+
+const multiply=num1*num2
+
+let random = Math.floor(Math.random() * (10 + 1)) + 0
+
+let mod = num3%num4
+
+let max = Math.max(18, 13, 19, 20, 8);
